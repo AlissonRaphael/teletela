819fc2154308539acd9a2fd2ddc2a9998bbbbb34
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-05-23 09:40:21 -0300

    feat: init state control

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
 import { Container, InputContainer } from "./style"
 
 export default function Input () {
+  const [input, setInput] = useState('')
+  const [question, setQuestion] = useState('')
+  const [response, setResponse] = useState('')
+
+  const handleChange = (event) => {
+    const { value } = event.target || ''
+
+    const [_question, _response ] = value.split('?')
+    setQuestion(_question+'?')
+    setResponse(_response)
+  }
+
   return (
     <Container>
       <InputContainer>
-        <input type="text"/>
+        <input type="text" onChange={(event) => handleChange(event)}/>
+        <div>Pergunta: {question}</div>
+        <div>Resposta: {response}</div>
       </InputContainer>
     </Container>
   )
