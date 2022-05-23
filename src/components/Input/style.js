224f01bb4cb815cea2f33f893af57bfbdcfa146e
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-05-23 09:39:58 -0300

    fixes: change input styles

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -13,10 +13,11 @@ export const InputContainer = styled.div`
     width: 40%;
 
   input {
-    font-size: 1.25rem;
-    background-color: rgb(200,200,200);
+    font-size: 1rem;
+    background-color: #fff;
     padding: 1rem;
-    border-radius: 0.25rem;
+    border-radius: 0.2rem;
     width: 100%;
+    outline: none;
   }
 `
