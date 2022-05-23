6c90671b423501b3058265ecb77d163cc289542b
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-05-23 09:41:34 -0300

    feat: create praise phrases

---
+++ src/components/Input/praise.js
@@ -0,0 +1,20 @@
+export const praise = [
+  {
+    shot: 'Meu grande líder',
+    medium: 'Meu grande líder revolucionário',
+    long: 'Meu grande líder revolucionário e estimado governante supremo',
+    extra: 'Meu grande líder revolucionário e estimado governante supremo, guia de toda essa nação'
+  },
+  {
+    shot: 'Oh! pai do pobres',
+    medium: 'Oh! pai do pobres e oprimidos',
+    long: 'Oh! pai do pobres e oprimidos dessa nossa grande nação',
+    extra: 'Oh! pai do pobres e oprimidos dessa nossa grande nação tão bem guiada pelas suas mãos'
+  },
+  {
+    shot: 'Grandioso líder',
+    medium: 'Grandioso líder supremo e íntegro',
+    long: 'Grandioso líder supremo e íntegro comandante revolucionário',
+    extra: 'Grandioso líder supremo e íntegro comandante revolucionário do nosso tão amador partido'
+  }
+]
