4636a841353ea01ebf332c911971a2a7c1540c76
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-05-24 09:51:33 -0300

    feat: add more praise phrase

--- src/components/Input/praise.js
+++ src/components/Input/praise.js
@@ -1,20 +1,26 @@
 export const praise = [
   {
-    shot: 'Meu grande líder',
+    short: 'Meu grande líder',
     medium: 'Meu grande líder revolucionário',
     long: 'Meu grande líder revolucionário e estimado governante supremo',
     extra: 'Meu grande líder revolucionário e estimado governante supremo, guia de toda essa nação'
   },
   {
-    shot: 'Oh! pai do pobres',
+    short: 'Oh! pai do pobres',
     medium: 'Oh! pai do pobres e oprimidos',
     long: 'Oh! pai do pobres e oprimidos dessa nossa grande nação',
     extra: 'Oh! pai do pobres e oprimidos dessa nossa grande nação tão bem guiada pelas suas mãos'
   },
   {
-    shot: 'Grandioso líder',
+    short: 'Grandioso líder',
     medium: 'Grandioso líder supremo e íntegro',
     long: 'Grandioso líder supremo e íntegro comandante revolucionário',
     extra: 'Grandioso líder supremo e íntegro comandante revolucionário do nosso tão amador partido'
+  },
+  {
+    short: 'Ah! Grande democrata',
+    medium: 'Ah! Grande democrata e estimado líder',
+    long: 'Ah! Grande democrata e estimado líder supremo da nossa nação',
+    extra: 'Ah! Grande democrata e estimado líder supremo da nossa nação tão bem guiada pelas suas mãos'
   }
 ]
