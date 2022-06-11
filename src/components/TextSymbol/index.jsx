b939c57a9ec13e69e4af22ecf966535764563ce4
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-06-11 19:47:16 -0300

    feat: create terminal symbol comp

---
+++ src/components/TextSymbol/index.jsx
@@ -0,0 +1,24 @@
+import { SymbolStyle } from './styles'
+
+export default function TerminalTextSymbol () {
+ return (
+  <SymbolStyle>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...',;;:cccccccc:;,..
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..,;:cccc::::ccccclloooolc;'.
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.',;:::;;;;:loodxk0kkxxkxxdocccc;;'..
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.,;;;,,;:coxldKNWWWMMMMWNNWWNNKkdolcccc:,.
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.',;;,',;lxo:...dXWMMMMMMMMNkloOXNNNX0koc:coo;.
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..,;:;,,,:ldl'&nbsp;&nbsp;&nbsp;.kWMMMWXXNWMMMMXd..':d0XWWN0d:;lkd,
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..,;;,,'':loc.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lKMMMNl.&nbsp;.c0KNWNK:&nbsp;&nbsp;..';lx00X0l,cxo,.
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..''....'cooc.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;c0NMMX;&nbsp;&nbsp;&nbsp;.l0XWN0;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,ddx00occl:.
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..'..&nbsp;&nbsp;.':odc.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.x0KKKkolcld000xc.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.cxxxkkdl:,..
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..''..&nbsp;&nbsp;&nbsp;;dxolc;'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.lxx000kkxx00kc.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.;looolllol:'..
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;..'..&nbsp;&nbsp;&nbsp;&nbsp;.':lloolc:,..&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'lxkkkkk0kd,&nbsp;&nbsp;&nbsp;..':clc:::;,,;:;,'..
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;......&nbsp;&nbsp;&nbsp;....',;;;:ccc::;;,''',:loddol:,,;:clllolc:;;,'........
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;....'''',,,;;:cccccclllloooollllccc:c:::;,'..
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.......'',,,,,,,,;;::::ccccc::::;;;,,''...
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;...............''',,,;;;,,''''''......
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;............................
+  </SymbolStyle>
+ )
+}
---
+++ src/components/TextSymbol/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const SymbolStyle = styled.div`
+  color: #68709d;
+  font-size: 0.8rem;
+  max-width: 25rem;
+`
