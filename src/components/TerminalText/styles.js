9a6b93383ab2064aac1fd4b9288b37ded26da5ae
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-06-11 19:46:54 -0300

    feat: create terminal text comp

---
+++ src/components/TerminalText/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Line = styled.p`
+  .user {
+    color: #FFFF80;
+  }
+
+  .dir {
+    color: #a4ffff;
+  }
+
+  .net {
+    color: #ff92df;
+  }
+`
