cc342c80d8ea7301c600ab35353275069da9267a
Alisson <alisson.oliveira@alunos.ufersa.edu.br>
2022-08-12 20:32:40 -0300

    refactor: change container comp

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react'
+import {motion, AnimatePresence } from 'framer-motion'
+
 import { ContainerStyle } from './style'
 import Input from '../Input'
+import Response from '../Response'
 import Logo from '../Logo'
 
 export default function Container () {
+  const [response, setResponse] = useState('')
+  const [isOk, setIsOk] = useState(false)
   const praiseNumber = Math.floor(Math.random() * 4)
 
   return (
     <ContainerStyle>
-      <Logo/>
-      <Input praiseNumber={praiseNumber}/>
+      <AnimatePresence>
+        <Logo color={ isOk ? 'red' : 'grey' }/>
+        { !isOk && <Input praiseNumber={praiseNumber} handleSetResponse={setResponse} handleSetIsOk={setIsOk}/> }
+        { isOk && <Response response={response}/> }
+        <motion.div
+          className="light"
+          animate={{ opacity: [0, 1, 1, 0, 1, 0, 0.5] }}
+          transition={{ repeat: Infinity, duration: 10 }}
+        />
+      </AnimatePresence>
     </ContainerStyle>
   )
 }
--- src/components/Container/style.js
+++ src/components/Container/style.js
@@ -8,4 +8,16 @@ export const ContainerStyle = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  position: relative;
+
+  div.light {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 10;
+    background-image: radial-gradient(rgba(255,0,0,0.1), rgba(255,0,0,0));
+  }
 `
